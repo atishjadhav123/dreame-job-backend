48ad49890fdaa1bae5f555be2f0cefec279f447c
atishe <you@example.com>
2025-04-15 21:45:07 +0530

    isauthenticated

--- routes/user.route.js
+++ routes/user.route.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router
     .post("/register", singleupload, register)
-    .post("/login", login)
+    .post("/login", isAuthenticated, login)
     .delete("/logout", logout)
     .put("/profile/update", isAuthenticated, singleupload, updateProfile)
 
