51f266691f1e1518bbbe0cdee8b848b668f5815b
atishe <you@example.com>
2025-04-16 13:26:20 +0530

    rename

--- routes/aplication.route.js
+++ routes/aplication.route.js
@@ -6,6 +6,6 @@ const router = express.Router()
 router
     .post("/applyjob/:id", isAuthenticated, applyJob)
     .get("/getapplyjob", isAuthenticated, getappliedJob)
-    .get("/:jobid/applicants", isAuthenticated, getAplicant)
+    .get("/:id/applicants", isAuthenticated, getAplicant)
     .put("/status/:id/update", isAuthenticated, updateStatus)
 export default router
