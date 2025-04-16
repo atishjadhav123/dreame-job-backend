1e6e08ea69337a29bd5b1c655b59f1800855f0df
atishe <you@example.com>
2025-04-16 11:55:06 +0530

    gitignore add

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
 const corsOptions = {
-    origin: "https://dreame-job-frountend-host.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true,
 }
 
